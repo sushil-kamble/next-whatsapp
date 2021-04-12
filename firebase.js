f9037369789b85ed4e030b37c083039fcad22bd9
Sushil <iamsushil303@gmail.com>
2021-04-12 17:54:41 +0530

    Fixed firebase import error

--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
